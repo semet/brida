2c0e4ef74664bbdaabd88c42e0764fba4d306208
Semet <hamdanilombok@gmail.com>
2022-08-19 18:09:14 +0800

    update breakpoints

--- config/theme.ts
+++ config/theme.ts
@@ -16,6 +16,13 @@ const theme = extendTheme({
 	fonts: {
 		body: `'Open Sans', sans-serif`,
 	},
+	breakPoints: {
+		sm: "320px",
+		md: "768px",
+		lg: "960px",
+		xl: "1200px",
+		"2xl": "1536px",
+	},
 });
 
 export default theme;
