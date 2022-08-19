9a165f2dcbc9fd6b0848a5a15e01bc134691ef34
Semet <hamdanilombok@gmail.com>
2022-08-19 18:08:43 +0800

    adjust style

--- components/header/MainHeader.tsx
+++ components/header/MainHeader.tsx
@@ -1,31 +1,41 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Text, Icon } from "@chakra-ui/react";
 import React from "react";
 import Image from "next/image";
 import NtbLogo from "../../public/images/logo/ntb.webp";
 import BridaLogo from "../../public/images/logo/brida.webp";
 import MainMenu from "./MainMenu";
+import { FaBars } from "react-icons/fa";
+import MobileMenu from "./MobileMenu";
 
 const MainHeader = () => {
 	return (
 		<Flex
-			height="50px"
+			height={{ sm: "9rem", lg: "6rem" }}
 			shadow="base"
-			paddingX="36"
+			paddingX={{ sm: "10", md: "20" }}
 			paddingY="12"
 			justify="space-between"
+			align="center"
+			direction={{ sm: "row", md: "column", lg: "row" }}
 		>
 			{/* Logo */}
 			<Flex align="center" gridGap="2">
-				<Image src={NtbLogo} alt="Nusa Tenggara Barat" />
+				<Box as="div">
+					<Image src={NtbLogo} alt="Nusa Tenggara Barat" />
+				</Box>
 				<Flex align="center">
-					<Image src={BridaLogo} alt="Nusa Tenggara Barat" />
-					<Text color="brida.100" display={{ sm: "none", md: "inline" }}>
+					<Box as="div">
+						<Image src={BridaLogo} alt="Nusa Tenggara Barat" />
+					</Box>
+					<Text color="brida.100" display={{ base: "none", lg: "block" }}>
 						BRIDA Provinsi NTB
 					</Text>
 				</Flex>
 			</Flex>
 			{/* Menu */}
 			<MainMenu />
+			{/* Mobile Menu */}
+			<MobileMenu />
 		</Flex>
 	);
 };
